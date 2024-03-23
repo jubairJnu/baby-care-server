9760e40725f372f0296a323c83bfbd3ce1694b74
jubairJnu <jubair2810@gmail.com>
2024-03-24 00:37:26 +0600

    fix minor issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@jubair2.qej77gj.mongodb.net/?retryWrites=true&w=majority&appName=Jubair2`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -72,7 +72,8 @@ async function run() {
     app.get("/products/:id", async (req, res) => {
       try {
         const { id } = req.params;
-        const result = await productCollections.findOne({ _id: id });
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollections.findOne(query);
         res.send(result);
       } catch (error) {
         res.send("Internal Server Error");
